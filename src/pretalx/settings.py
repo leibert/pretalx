7313761cf04b0ff5f1aa13584b87ac44852e508c
leibert <cwm@eml.cc>
2020-07-19 00:19:21 +0000

    cache debug

--- src/pretalx/settings.py
+++ src/pretalx/settings.py
@@ -155,6 +155,7 @@ else:
 
 ## TASK RUNNER SETTINGS
 HAS_CELERY = bool(config.get("celery", "broker", fallback=None))
+HAS_CELERY = False
 if HAS_CELERY:
     CELERY_BROKER_URL = config.get("celery", "broker")
     CELERY_RESULT_BACKEND = config.get("celery", "backend")
@@ -259,10 +260,11 @@ else:
 
 ## CACHE SETTINGS
 CACHES = {"default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache"}}
-REAL_CACHE_USED = False
+REAL_CACHE_USED = True
 SESSION_ENGINE = None
 
 HAS_MEMCACHED = bool(os.getenv("PRETALX_MEMCACHE", ""))
+
 if HAS_MEMCACHED:
     REAL_CACHE_USED = True
     CACHES["default"] = {
