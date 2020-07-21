152535dc65adf72ddd5bc8c420af2d7cb719a607
leibert <cwm@eml.cc>
2020-07-21 12:39:51 +0000

    fix

--- src/pretalx/agenda/urls.py
+++ src/pretalx/agenda/urls.py
@@ -68,7 +68,7 @@ urlpatterns = [
                     speaker.SpeakerRedirect.as_view(),
                     name="speaker.redirect",
                 ),
-                url(r"^workshops/$", talk.WorkshopList.as_view(), name="workshops"),
+                url(r"^workshops/$", talk.PerformanceList.as_view(), name="workshops"),
                 url(r"^performances/$", talk.PerformanceList.as_view(), name="performances"),
                 url(r"^talk/$", talk.TalkList.as_view(), name="talks"),
                 # url(r"^talk_hope/$", talk.TalkList_hope.as_view(), name="talks_hope"),
